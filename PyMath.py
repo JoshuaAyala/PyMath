07f0533769ce0c40ac3599b40cb2d1d6eb25e0f3
Joshua <Joshua4yala@pm.me>
2020-12-06 21:01:31 -0800

    Update PyMath.py
    Se agregaron nuevas funciones, y la existencia de módulos.

--- PyMath.py
+++ PyMath.py
@@ -1,17 +1,106 @@
 #!/usr/bin/python
-# coding=utf-8
+#coding: utf-8
 
 # Filename  : PyMath.py
 # Autor     : Joshua Ayala
 # Website   : https://github.com/JoshuaAyala/PyMath
+# pep8: 100%
 
 #-----importaciones-----
-import os, math, platform
+from packages.operaciones import *
+from packages.banners import *
+
+import os, math, platform, time
 
 # declaración de variables
 decision = 0
 OSsystem = platform.system()
 
+# Menú Principal
+def menu(decision):
+    decision = int(input("Selecciona una opción:\n[1] Aritmetica.\n[2] Física.\n[3] Áreas.\n\n[0] Salir.\n\n>: "))
+    if (decision==1):
+        aritmeticaMenu(decision)
+    elif(decision==2):
+        fisicaMenu(decision)
+    elif(decision==3):
+        areasMenu(decision)
+    elif(decision==0):
+        os.system('exit')
+    else:
+        print("Error")
+        goBack()
+
+# Menú de la opción [1] Aritmética
+def aritmeticaMenu(decision):
+    checkOSandCleanohYeah()
+    aritmeticaBanner()
+    subdecision = int(input("Selecciona una opción´:\n[1] Suma.\n[2] Resta.\n[3] División.\n[4] Multiplicación.\n\n[0] Volver.\n\n>: "))
+    if(subdecision == 1):
+        Sumar()
+        goBack()
+    elif(subdecision == 2):
+        Restar()
+        goBack()
+    elif(subdecision == 3):
+        Dividir()
+        goBack()
+    elif(subdecision == 4):
+        Multiplicar()
+        goBack()
+    elif(subdecision == 0):
+        main()
+    else:
+        print("Opción equivocada")
+        time.sleep(2)
+        aritmeticaMenu(decision)
+
+# Menú de la opción [2] Física
+def fisicaMenu(decision):
+    checkOSandCleanohYeah()
+    fisicaBanner()
+    subdecision = int(input("Selecciona una opción:\n[1] Velocidad.\n[2] Distancia.\n[3] Tiempo.\n\n[0] Volver.\n\n>: "))
+    if(subdecision == 1):
+        fisicaVelocidad()
+        goBack()
+    elif(subdecision == 2):
+        fisicaDistancia()
+        goBack()
+    elif(subdecision == 3):
+        fisicaTiempo()
+        goBack()
+    elif(subdecision == 0):
+        main()
+    else:
+        print("Error")
+        time.sleep(1)
+        fisicaMenu(decision)
+
+# Menú de la opción [3] Áreas
+def areasMenu(decision):
+    checkOSandCleanohYeah()
+    areasBanner()
+    subdecision = int(input("Selecciona una opción´:\n[1] Triángulo.\n[2] Círculo.\n[3] Cuadrado.\n[4] Rectángulo.\n\n[0] Volver.\n\n>: "))
+    if(subdecision == 1):
+        areaTriangulo()
+        goBack()
+    elif(subdecision == 2):
+        areaCirculo()
+        goBack()
+    elif(subdecision == 3):
+        areaCuadrado()
+        goBack()
+    elif(subdecision == 4):
+        areaRectangulo()
+        goBack()
+    elif(subdecision == 0):
+        main()
+    else:
+        print("Error")
+        time.sleep(1)
+        areasMenu(decision)
+
+# Función para regresar a main
 def goBack():
     print("")
     goBack = int(input("¿Quieres regresar al menu?\n[1] Sí.\n[0] No.\n\n>:"))
@@ -21,115 +110,21 @@ def goBack():
         exit()
     else:
         print("Error.")
-
-def banner ():
-    print("+==============================================+")
-    print("+                                              +")
-    print("+      ▄▄▄· ▄· ▄▌• ▌ ▄ ·.  ▄▄▄· ▄▄▄▄▄ ▄ .▄     +")
-    print("+     ▐█ ▄█▐█▪██▌·██ ▐███▪▐█ ▀█ •██  ██▪▐█     +")
-    print("+      ██▀·▐█▌▐█▪▐█ ▌▐▌▐█·▄█▀▀█  ▐█.▪██▀▐█     +")
-    print("+     ▐█▪·• ▐█▀·.██ ██▌▐█▌▐█ ▪▐▌ ▐█▌·██▌▐▀     +")
-    print("+     .▀     ▀ • ▀▀  █▪▀▀▀ ▀  ▀  ▀▀▀ ▀▀▀ ·     +")
-    print("+=================Joshua Ayala=================+")
-    print("")
-    
-def menu():
-    print("Selecciona una opción:")
-    print("[1] Sumar/Multiplicar.")
-    print("[2] Separación de números pares e impares.")
-    print("[3] Áreas (círculo, rectángulo).\n")
-    print("[0] Salir.")
-    decision = int(input(">: "))
-    if (decision==1):
-        sumarMultiplicar()
-    elif(decision==2):
-        separarNum()
-    elif(decision==3):
-        areas()
-    elif(decision==0):
-        restart=False
-        os.system('exit')
-    else:
-        restart=False
-        print("Error")
-
-
-def sumarMultiplicar():
-    print("¿Quieres sumar o multiplicar?\n")
-    print("[1] Sumar.")
-    print("[2] Multiplicar.\n")
-    print("[0] Regresar.")
-    decision = int(input(">: "))
-    if (decision==1):
-        limite = int(input("¿Cuántos números quieres sumar?: "))
-        resultado=0
-        i=0
-        numero = 0
-        while(i<limite):
-            i+=1
-            numero = float(input("Introduce un valor: "))
-            resultado = resultado + numero
-            
-        print("El resultado de la suma es: ", resultado) 
+        time.sleep(1)
         goBack()
-        
-    elif (decision==2):
-        numero = int(input("Introduce el numero del que quieras saber su tabla de multiplicación: "))
-        limite = int(input("¿Hasta donde quieres multiplicarlo?: "))
-        i=0
-        while(i<limite):
-            i+=1
-            print(numero, "x", i, "=", numero*i)
 
-        goBack()
-    elif (decision==0):
-        main()
-    else:
-        print("Error.")
-
-def separarNum():
-    i=0
-    impares = []
-    pares = []
-    limite = int(input("Introduce hasta donde quieres separar los numeros:\n>: "))
-    while(i<limite):
-        i+=1
-        if(i%2==0):
-            pares.append(i)
-        else:
-            impares.append(i)
-
-    print("Pares: ", pares)
-    print("")
-    print("Impares: ", impares)
-    goBack()
-def areas():
-    print("¿Quieres calcular el área de un círculo o la de un rectángulo?\n")
-    print("[1] Círculo.")
-    print("[2] Rectángulo.\n")
-    print("[0] Regresar.")
-    decision = int(input(">: "))
-    if (decision==1):
-        radio = int(input("Introduce el valor del radio: "))
-        area = (radio**2)*math.pi
-        print("El area del circulo es: ", area)
-    elif (decision==2):
-        base = int(input("Introduce el valor de la base: "))
-        altura = int(input("Introduce el valor de la altura: "))
-        area = base * altura
-        print("El area del rectangulo es: ", area)
-    elif (decision==0):
-        main()
-    else:
-        print("Error.")
-
-    goBack()
-def main():
-    if(OSsystem=="Linux" or OSsystem=="MacOS"):
+# Saber que sistema operativo se usa y saber qué comando usar
+def checkOSandCleanohYeah():
+    if(OSsystem=="Linux" or OSsystem=="Darwin" or OSsystem=="MacOS"):
         os.system('clear')
     else:
         (os.system('cls'))
-    banner()
-    menu()
 
-main()
+# Función main
+def main():
+    checkOSandCleanohYeah()
+    mainbanner()
+    menu(decision)
+
+if __name__ == '__main__':
+    main()
